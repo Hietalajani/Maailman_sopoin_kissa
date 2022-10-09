2af0c4b807c8da75766a7848c778b62c14127703
Jani Stolt <jani.stolt@metropolia.fi>
2022-10-09 22:54:30 +0300

    peliluuppi toimii ja funktiot testausystävällisemmiksi

--- Pääohjelma.py
+++ Pääohjelma.py
@@ -3,25 +3,26 @@ import mysql.connector
 import random
 from geopy import distance
 
-# Muodostetaan yhteys tietokantaan
-salasana = input("Anna salasana tietokantaan.")
-yhteys = mysql.connector.connect(
-    host='localhost',
-    port=3306,
-    database='flight_game',
-    user='root',
-    password=salasana,
-    autocommit=True
-)
 
-# Tyhjä sanakirja lentokentille
-lentokentät = {}
+# Muodostetaan yhteys tietokantaan
+# Funktio palauttaa luomansa yhteyden
+def luo_yhteys():
+    salasana = input("Anna salasana tietokantaan.")
+    uusi_yhteys = mysql.connector.connect(
+        host='localhost',
+        port=3306,
+        database='flight_game',
+        user='root',
+        password=salasana,
+        autocommit=True)
+    return uusi_yhteys
 
 
 # Tällä funktiolla haetaan lentokenttien ICAO-koodit ja asetetaan niille arvoksi kyseisellä kentällä oleva
-# asia: herkkutikku, tonnikala, kissanminttu tai kissa. Tallennetaan ICAOt ja niiden arvot sanakirjaan lentokentät.
+# asia: herkkutikku, tonnikala, kissanminttu tai kissa.
+# Tallennetaan ICAOt ja niiden arvot parametrina annettuun sanakirjaan.
 # Funktio palauttaa sanakirjan.
-def kenttienarvot():
+def kenttienarvot(kentät, yhteys):
     # Mielekkyyden vuoksi otetaan vain yksi lentokenttä per maa, siksi group by iso_country
     sql = '''select ident from airport 
     where continent = "EU" and type = "balloonport" and not iso_country = "RU" 
@@ -29,11 +30,11 @@ def kenttienarvot():
     kursori = yhteys.cursor()
     kursori.execute(sql)
     icaot = kursori.fetchall()
-    #   print(len(icaot))
-    kissaluku = random.randint(0, 35)
+    #   print(len(icaot))   # testiprintti
+    kissaluku = random.randint(0, len(icaot) - 1)
 
     # Kissa sujahtaa lentokentälle
-    lentokentät[icaot[kissaluku][0]] = 'Kissa'
+    kentät[icaot[kissaluku][0]] = 'Kissa'
 
     # Määritetään 6 lentokenttää joihin tulee herkkutikku
     for kierros in range(6):
@@ -42,8 +43,8 @@ def kenttienarvot():
             kentänarvo = random.randint(0, 35)
             if kentänarvo == kissaluku:
                 continue
-            elif icaot[kentänarvo][0] not in lentokentät:
-                lentokentät[icaot[kentänarvo][0]] = 'Herkkutikku'
+            elif icaot[kentänarvo][0] not in kentät:
+                kentät[icaot[kentänarvo][0]] = 'Herkkutikku'
                 successful = True
 
     # 5 lentokenttää joihin tulee tonnikala
@@ -53,8 +54,8 @@ def kenttienarvot():
             kentänarvo = random.randint(0, 35)
             if kentänarvo == kissaluku:
                 continue
-            elif icaot[kentänarvo][0] not in lentokentät:
-                lentokentät[icaot[kentänarvo][0]] = 'Tonnikala'
+            elif icaot[kentänarvo][0] not in kentät:
+                kentät[icaot[kentänarvo][0]] = 'Tonnikala'
                 successful = True
 
     # ja 4 lentokenttää joihin tulee kissanminttu
@@ -64,15 +65,15 @@ def kenttienarvot():
             kentänarvo = random.randint(0, 35)
             if kentänarvo == kissaluku:
                 continue
-            elif icaot[kentänarvo][0] not in lentokentät:
-                lentokentät[icaot[kentänarvo][0]] = 'Kissanminttu'
+            elif icaot[kentänarvo][0] not in kentät:
+                kentät[icaot[kentänarvo][0]] = 'Kissanminttu'
                 successful = True
 
+    return kentät
 
-    return lentokentät
 
 # Läheiset_lentokentät tuottaa kaikki lentokentät alle 500km päässä nykyisestä (nimet, ei ICAO:t)
-def läheiset_lentokentät(location):
+def läheiset_lentokentät(location, yhteys):
     läheiset = []
     nykyisetkoordinaatit = f'''select latitude_deg, longitude_deg
     from airport where ident = "{location}";'''
@@ -90,28 +91,29 @@ def läheiset_lentokentät(location):
             pass
     return läheiset
 
+
 # Uuden kentän nimi takaisin ICAO:ksi, tän ongelman voi varmaan jotenkin välttää lol
-def icaoksi(nimi):
+def icaoksi(nimi, yhteys):
     sql = f'select ident from airport where name = "{nimi}";'
     kursori = yhteys.cursor()
     kursori.execute(sql)
     icao = kursori.fetchone()
     return icao[0]
 
+
 # Herkuntarkistus tarkistaa, onko uudella lentokentällä herkkua tai kissa, palauttaa herkun nimen
-def herkuntarkistus(nimi):
-    if nimi in lentokentät:
-        print(f'Löytyi {lentokentät[nimi]}')
-        return lentokentät[nimi]
+def herkuntarkistus(nimi, kentät):
+    if nimi in kentät:
+        print(f'Löytyi {kentät[nimi]}')
+        return kentät[nimi]
     else:
         print('Ei löytynyt mitään :(')
         return "none"
 
 
-# Määritellään kissan kärsivällisyys ja sen hiipuminen/nouseminen, annetaan herkulle muuttuja rekursioiden välttämiseksi
-
-kisun_kärsivällisyys = 20000
-def kärsivällisyyshiipuu(vanhalocation, uusilocation):
+# Määritellään kissan kärsivällisyyden hiipuminen/nouseminen,
+# annetaan herkulle muuttuja rekursioiden välttämiseksi
+def kärsivällisyyshiipuu(vanhalocation, uusilocation, yhteys, kärsivällisyys):
     kenttä1 = f'''select latitude_deg, longitude_deg from airport
     where ident = "{vanhalocation}";'''
     kenttä2 = f'''select latitude_deg, longitude_deg from airport
@@ -122,39 +124,51 @@ def kärsivällisyyshiipuu(vanhalocation, uusilocation):
     kursori.execute(kenttä2)
     k2 = kursori.fetchall()
     matka = int(distance.distance(k1, k2).km)
-    global kisun_kärsivällisyys
     global herkku
     if herkku != "none":
         if herkku == 'Herkkutikku':
-            kisun_kärsivällisyys += 500
+            kärsivällisyys += 500
             print('Kissan kärsivällisyys kasvoi 500!')
         elif herkku == 'Tonnikala':
-            kisun_kärsivällisyys += 750
+            kärsivällisyys += 750
             print('Kissan kärsivällisyys kasvoi 750!')
         elif herkku == 'Kissanminttu':
-            kisun_kärsivällisyys += 1000
+            kärsivällisyys += 1000
             print('Kissan kärsivällisyys kasvoi 1000!')
-    kisun_kärsivällisyys -= matka
+    kärsivällisyys -= matka
     print(f'Matkan keston takia kissan kärsivällisyys laski {matka}!')
-    return
+    return kärsivällisyys
 
-# Gameplay loop
 
-# Peli arpoo kenttien arvot
-kenttienarvot()
+if __name__ == '__main__':
+    # luodaan yhteys
+    peliyhteys = luo_yhteys()    # huomaa vastata salasanakyselyyn konsolissa
 
-# Peli aloitetaan aina samasta paikasta? Vaikka Helsinki?
-icao1 = 'EFHK'
+    # Tyhjä sanakirja lentokentille
+    lentokentät = {}
+    # määritellään kisulle kärsivällisyysarvo
+    kisun_kärsivällisyys = 20000
 
-# Peli tarjoaa käyttäjälle läheisiä lentokenttiä joihin voi lentää
-print(f'Tässä läheisimmät kentät: {läheiset_lentokentät(icao1)}')
+    # Peli arpoo kenttien arvot
+    kenttienarvot(lentokentät, peliyhteys)
 
-# Käyttäjä valitsee mille lentokentälle lentää
-minne = int(input('Valitse mille lentokentälle haluat lentää seuraavaksi indeksiluvulla: '))
-lentokenttä2 = läheiset_lentokentät(icao1)[minne]
-icao2 = icaoksi(lentokenttä2)
-herkku = herkuntarkistus(icao2)
+    # Peli aloitetaan aina samasta paikasta? Vaikka Helsinki?
+    icao1 = 'EFHK'
 
-# Kärsivällisyys hiipuu/nousee
-kärsivällisyyshiipuu(icao1, icao2)
-print(f'Kissan kärsivällisyyttä jäljellä: {kisun_kärsivällisyys}')
+    # Gameplay loop
+    while kisun_kärsivällisyys > 0:
+        # Peli tarjoaa käyttäjälle läheisiä lentokenttiä joihin voi lentää
+        print(f'Tässä läheisimmät kentät: {läheiset_lentokentät(icao1, peliyhteys)}')
+
+        # Käyttäjä valitsee mille lentokentälle lentää
+        minne = int(input('Valitse mille lentokentälle haluat lentää seuraavaksi indeksiluvulla: '))
+        lentokenttä2 = läheiset_lentokentät(icao1, peliyhteys)[minne]
+        icao2 = icaoksi(lentokenttä2, peliyhteys)
+        herkku = herkuntarkistus(icao2, lentokentät)
+
+        # Kärsivällisyys hiipuu/nousee
+        kisun_kärsivällisyys = kärsivällisyyshiipuu(icao1, icao2, peliyhteys, kisun_kärsivällisyys)
+        print(f'Kissan kärsivällisyyttä jäljellä: {kisun_kärsivällisyys}\n')
+
+        # muutetaan uusi sijainti nykyiseksi sijainniksi
+        icao1 = icao2
---
+++ testaus.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class Testit(unittest.TestCase):
+    def testitesti(self):
+        self.assertEqual(0, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
