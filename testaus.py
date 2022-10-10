fd7cb336db61627fe1d6b67badf5ae336e31f4ed
Jani Stolt <jani.stolt@metropolia.fi>
2022-10-10 14:30:10 +0300

    esimerkkitesti

--- testaus.py
+++ testaus.py
@@ -2,8 +2,11 @@ import unittest
 
 
 class Testit(unittest.TestCase):
-    def testitesti(self):
-        self.assertEqual(0, False)  # add assertion here
+    def testaa_pariton(self):
+        luku = int(input("Anna pariton luku."))
+        tulos = (luku % 2 == 1)
+        print(tulos)
+        self.assertEqual(tulos, True)  # add assertion here
 
 
 if __name__ == '__main__':
