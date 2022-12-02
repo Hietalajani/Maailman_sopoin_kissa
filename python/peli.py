367e82c4f24865891cdb6d0c33566c2605a0f19c
pankeeki <sannimo@hotmail.com>
2022-12-02 13:03:48 +0200

    luokat

---
+++ python/peli.py
@@ -0,0 +1,9 @@
+class Peli:
+    def __init__(self, pelaajanimi, peliid, sijainti, karsivallisyyskulunut, tila, aiempisijainti):
+        self.pelaajanimi = pelaajanimi
+        self.peliid = peliid
+        self.sijainti = sijainti
+        self.karsivallisyyskulunut = karsivallisyyskulunut
+        self.tila = tila
+        self.aiempisijainti = aiempisijainti
+    def metodi(self):
