855b615d4da5d9b3a0973782faf179eb86ab954b
Jani Stolt <101009270+Lirjants@users.noreply.github.com>
2022-12-02 13:25:42 +0200

    tietokantayhteys tiedoston alkuun

--- python/game.py
+++ python/game.py
@@ -6,16 +6,18 @@ import mysql.connector
 from flask import Flask
 
 app = Flask(__name__)
+pw = input("Anna tietokannan salasana:")
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='kisupeli2',
+    user='root',
+    password=pw,
+    autocommit=True
+)
+
+def hae_tietokannasta():
+    pass
 
 if __name__ == "__main__":
-    pw = input("Anna tietokannan salasana:")
-    yhteys = mysql.connector.connect(
-        host='127.0.0.1',
-        port=3306,
-        database='kisupeli2',
-        user='root',
-        password=pw,
-        autocommit=True
-    )
-
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
