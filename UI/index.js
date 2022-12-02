a3d3c281a674176d04eb23e1f0f95927af1c2963
Hietalajani <hietsuj@gmail.com>
2022-12-02 14:03:21 +0200

    ohjeet ja pistetaulukko tehty, yhteyttä pythoniin ei vielä luotu

--- UI/index.js
+++ UI/index.js
@@ -1,10 +1,43 @@
-const ohjeet = document.querySelector('#ohjeet');
+const ohjeet = document.getElementById('ohjeet');
 const pisteet = document.querySelector('#pisteet');
-const dialog = document
+const dialog = document.querySelector('main').
+    appendChild(document.createElement('dialog'));
 
-[ohjeet, pisteet].forEach(item => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
+ohjeet.addEventListener('click', (e) => {
+  e.preventDefault();
 
-  });
+  const txt = new XMLHttpRequest();
+  txt.open('GET', 'ohjeet.txt');
+  txt.onreadystatechange = () => {
+    const text = txt.responseText;
+    dialog.innerText = text;
+  };
+  txt.send();
+
+  dialog.showModal();
+});
+
+pisteet.addEventListener('click', async function(e) {
+  e.preventDefault();
+
+  const highscore = await fetch('pythonurlitähän');
+  const resp = await highscore.json();
+  let pelaajalista = [];
+
+  for (let pelaaja of resp) {
+    const pelaajaobjekti = {
+      spot: pelaaja.spot,
+      name: pelaaja.name,
+      score: pelaaja.score,
+    };
+
+    pelaajalista.push(pelaajaobjekti)
+
+    const table = new Tabulator("dialog", {
+    data: pelaajalista,
+    autoColumns: true,
+});
+
+  }
+
 });
