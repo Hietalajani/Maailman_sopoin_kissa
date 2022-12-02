35dcfb881b2914cc183132052490aee16e0ffbb3
Jani Stolt <101009270+Lirjants@users.noreply.github.com>
2022-12-02 13:11:43 +0200

    tietokantayhteys ja Flaskin alku

--- python/game.py
+++ python/game.py
@@ -1,4 +1,21 @@
-from esine.py import Esine
-from tavoite.py import Tavoite
-from peli.py import Peli
-from kentta.py import Kentta
+from esine import Esine
+from tavoite import Tavoite
+from peli import Peli
+from kentta import Kentta
+import mysql.connector
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    pw = input("Anna tietokannan salasana:")
+    yhteys = mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        database='kisupeli2',
+        user='root',
+        password=pw,
+        autocommit=True
+    )
+
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
